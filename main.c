a30b59bd65e3bbe9e092c0db928739a3e09ad9f1
fite-or-flite <62438900+fite-or-flite@users.noreply.github.com>
2020-07-01 14:40:38 -0500

    reworked with pointers
    seems to all work, but needs menu back

--- main.c
+++ main.c
@@ -1,164 +1,99 @@
-//program to establish common card deck functions
-//too many globals; need to rewrite using pointers
-
-
-//sortcards using cardsinhand only; not ccardsinhand
-
+//yet another rewritten deck of cards program
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-int deck[52];
-int cardsInHand = 7;
-int cCardsInHand = 7;
-int dcounter = 0;
-
-
-int hand[52];
-int compHand[52];
+void shuffleDeck(int*);
+void printDeck(int*);
+void printHand(int*, int);
+void drawCard(int*, int, int*);
+void sortCards(int*, int);
 
 char suit[4] = {'C', 'D', 'S', 'H'};
 char face[13] = {'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A'};
 
-void initDeck();
-void shuffleDeck();
-void printDeck();
-void printCard(int);
-void dealHand();
-void printHand(int []); //for printing player's hand; problems if comp and player dont have same no of cards
-void drawCard(int []);
-void cDrawCard(int []);
-void sortCards(int [], int);
 
-int main() {
+int main()
+{
+    int deck[52] = {0};
+    int pHand[52] = {0};
+    int cHand[52] = {0};
+    int pNumCards = 0;
+    int cNumCards = 0;
+    int *deckPosition = deck;
 
-    int cardNum;
+    //populate deck
+    for (int i = 0; i < 52; i++)
+        deck[i] = i;
 
-    //getting deck ready
-    initDeck();
-    printDeck();
-    shuffleDeck();
-    printDeck();
-    dealHand();
+    shuffleDeck(deck);
+    printDeck(deck);
 
-    printf("\n-----------------------\n");
+    //deal hands
+    //could have made into function, but then can't change deckposition or xnumcards values
+    printf("\nDealing 7 cards each.\n");
+    for (int n = 0; n < 7; n++) {
+        pHand[n] = *deckPosition;
+        deckPosition++;
+        pNumCards++;
 
-    printf("\nYour hand:");
-    printHand(hand);
-    printf("\n\t   ");
-    for (int n = 0; n < cardsInHand; n++)
-        printf(" %d", hand[n]);
+        cHand[n] = *deckPosition;
+        deckPosition++;
+        cNumCards++;
+    }
 
-    sortCards(hand, cardsInHand);
-    printf("\nYour hand:");
-    printHand(hand);
-    printf("\n\t   ");
-    for (int n = 0; n < cardsInHand; n++)
-        printf(" %d", hand[n]);
+    //print hands
+    printf("\nYou:");
+    printHand(pHand, pNumCards);
+    printf("\nComputer:");
+    printHand(cHand, cNumCards);
 
+    //draw a card
+    drawCard(pHand, pNumCards, deckPosition);
+    pNumCards++; //couldn't change value inside function
+    deckPosition++; //maybe add some pointers to pass instead?
 
-    printf("\nComputer's hand: ");
-    printHand(compHand);
-    printf("\n\t\t");
-    for (int k = 0; k < cCardsInHand; k++)
-        printf(" %d ", compHand[k]);
-    sortCards(compHand, cCardsInHand);
-    printf("\nComputer's hand: ");
-    printHand(compHand);
-    printf("\n\t\t");
-    for (int k = 0; k < cCardsInHand; k++)
-        printf(" %d ", compHand[k]);
-    printf("\n-----------------------\n");
-    cardNum = 51;
-    printf("\nCard number %d is: ", cardNum);
-    printCard(cardNum);
-    printf("\n-----------------------\n");
-
-    printf("\nDrawing a card\n");
-    drawCard(hand);
-    cDrawCard(compHand);
-
-    printf("\nYour hand:");
-    sortCards(hand, cardsInHand);
-    printHand(hand);
-    printf("\n\t   ");
-    for (int n = 0; n < cardsInHand; n++)
-        printf(" %d", hand[n]);
-    printf("\nComputer's hand: ");
-    sortCards(compHand, cCardsInHand);
-    printHand(compHand);
-    printf("\n\t\t");
-    for (int p = 0; p < cCardsInHand; p++)
-        printf(" %d", compHand[p]);
-
-    printf("\n-----------------------\n");
+    printHand(pHand, pNumCards);
 
     return 0;
 }
 
-void initDeck(){
-//initialize deck
-//starting at 0 for 2-clubs, 1 for 2-diamonds, 2 for 2-spades,...51 for Ace-Hearts
-    for (int i = 0; i < 52; i++)
-        deck[i] = i;
-}
-
-void shuffleDeck(){
-
+void shuffleDeck(int deck[]) {
+    printf("\n...Shuffling...\n");
     srand(time(NULL));
     int y, temp;
 
-    printf("\n...shuffling...\n");
-
-    for (int x = 51; x > 0; x--) { //x has to be numCards - 1; otherwise get two zeros
-            y = rand() % x; //gets rando num bet 0 & 52
+    for (int x = 51; x > 0; x--) {
+            y = rand() % x;
             temp = deck[x];
             deck[x] = deck[y];
             deck[y] = temp;
     }
-}
-
-void printDeck(){
-    printf("\n");
-    for (int i = 0; i < 52; i++)
-        printf(" %d ", deck[i]);
-    printf("\n");
 
 }
 
-void printCard(int cardNum){
-    printf("%c-%c", face[cardNum / 4], suit[cardNum % 4]);
-}
-
-void dealHand() {
-
-    for (int j = 0; j < cardsInHand; j++) {
-        hand[j] = deck[dcounter];
-        dcounter++;
-        compHand[j] = deck[dcounter];
-        dcounter++;
+void printDeck(int deck[]) {
+    printf("Your deck: ");
+    for (int j = 0; j < 52; j++){
+        printf(" %d ", deck[j]);
     }
+
 }
 
-void printHand(int hand[]) {
-    //sortCards(hand, cardsInHand);
-    for (int i = 0; i < cardsInHand; i++)
+void printHand(int hand[], int numCards) {
+    sortCards(hand, numCards);
+    printf("\nHand: ");
+    for (int i = 0; i < numCards; i++) {
         printf(" %c-%c ", face[hand[i] / 4], suit[hand[i] % 4]);
+
+    }
+    printf("\n");
 }
 
-void drawCard(int hand[]) {
-    hand[cardsInHand] = deck[dcounter];
-    cardsInHand++;
-    dcounter++;
- }
-
-void cDrawCard(int compHand[]) {
-    compHand[cCardsInHand] = deck[dcounter];
-    cCardsInHand++;
-    dcounter++;
-
+void drawCard(int hand[], int numCards, int *deckPosition) {
+    printf("\n...Drawing a card...\n");
+    hand[numCards] = *deckPosition; //numCards is the next open space in the array, not last element
 }
 
 void sortCards(int hand[], int cardsInHand) {
